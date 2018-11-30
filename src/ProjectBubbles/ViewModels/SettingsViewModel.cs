eb4f4bbd42761384af895bd934e8fe932ed06bad
Sebastien Bovo <sebastienbovo@outlook.com>
2018-11-30 17:00:44 +0100

    SettingsPage

---
+++ src/ProjectBubbles/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ProjectBubbles.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+
+
+        
+
+        public SettingsViewModel()
+        {
+            Title = "Settings";
+            SaveSettingsCommand = new Command(async () => await SaveSettings());
+        }
+
+        async Task SaveSettings()
+        {
+            // TODO: Save the username to the "localstate" using Xamarin shared code
+            Debug.WriteLine(SavedUserName);
+
+        }
+
+        public ICommand SaveSettingsCommand { get; }
+        public string SavedUserName { get; set; }
+    }
+}
---
+++ src/ProjectBubbles/Views/SettingsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ProjectBubbles.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectBubbles.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        SettingsViewModel viewModel;
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+            viewModel = new SettingsViewModel();
+            BindingContext = viewModel;
+        }
+    }
+}
