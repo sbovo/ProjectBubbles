61c54cf69d1f225fdbe34bf23647f91a23fea59c
Sebastien Bovo <sebastienbovo@outlook.com>
2018-12-14 16:16:43 +0100

    Add skeleton for Profiles on AzureTable

---
+++ src/Models/Profile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectBubbles.Models
+{
+    public class Profile
+    {
+        public string UserId { get; set; }
+        public string USername { get; set; }
+        public string PhotoBase64Encoded { get; set; }
+    }
+}
